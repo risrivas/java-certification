365ba44930663409d19cea6eea589a887527de14
Rishi Srivastava <rishisrivasta@gmail.com>
2022-01-15 04:01:56 +0800

    started path

--- ocp11/manipulating-files/src/main/java/org/paumard/files/PlayWithPath.java
+++ ocp11/manipulating-files/src/main/java/org/paumard/files/PlayWithPath.java
@@ -6,15 +6,29 @@ import java.nio.file.Paths;
 public class PlayWithPath {
 
     public static void main(String[] args) {
+        Path path1 = Paths.get("c:/images/seaside.jpg");
 
-        Path path1 = Paths.get("videos/");
-        Path path2 = Path.of("images/pngs/");
+        // new in Java 11
+        Path path2 = Path.of("mountain.jpg");
 
         System.out.println("path1 = " + path1);
         System.out.println("path2 = " + path2);
         System.out.println("path1 = " + path1.getClass());
 
-        Path relativize = path1.relativize(path2);
-        System.out.println("relativize = " + relativize);
+        boolean absolute = path1.isAbsolute();
+        System.out.println("absolute = " + absolute);
+
+        Path root = path1.getRoot();
+        System.out.println("root = " + root);
+
+        // resolve - concat path1 and path2
+        // if path2 is absolute, then no concat and only path2 is returned
+        Path resolve = path1.resolve(path2);
+        System.out.println("resolve = " + resolve);
+
+        // resolve sibling - concat parent of path1 with path2
+        // if path1 parent is null, then only return path2
+        Path resolveSibling = path1.resolveSibling(path2);
+        System.out.println("resolveSibling = " + resolveSibling);
     }
 }
