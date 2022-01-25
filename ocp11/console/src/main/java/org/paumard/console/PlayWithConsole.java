3b1702162c7b20a261ea4f5f2dac5591780e19d9
Rishi Srivastava <rishisrivasta@gmail.com>
2022-01-25 08:31:13 +0800

    finished java io pluralsight

---
+++ ocp11/console/src/main/java/org/paumard/console/PlayWithConsole.java
@@ -0,0 +1,35 @@
+package org.paumard.console;
+
+import java.io.Console;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class PlayWithConsole {
+
+    public static void main(String[] args) {
+
+        Console console = System.console();
+
+        PrintWriter writer = console.writer();
+        writer.printf("What is your name? > ");
+
+        String name = console.readLine();
+        writer.printf("Hello %s\n", name);
+        writer.printf("What is your password %s? ", name);
+
+        char[] chars = console.readPassword();
+        String password = new String(chars);
+        writer.printf("%s is an easy password to guess!\n", password);
+        writer.printf("What are your favorite colors %s? ", name);
+        String colors = console.readLine();
+
+        List<String> providedColors =
+                new Scanner(colors)
+                        .useDelimiter(" ")
+                        .tokens()
+                        .collect(Collectors.toList());
+        writer.println(providedColors);
+    }
+}
